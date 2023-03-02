36800d406b87f6b4b980723c42e9895e639b3158
Anuj Shrestha <anuj@studiomatrix.com.np>
2023-03-02 15:27:29 +0545

    feat(HeroContainer): add a hero container on home page

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -1,13 +1,44 @@
 <template>
-
+  <header class="hero">
+    <div class="hero-container">
+      <div class="hero-text">
+        <h1>Simply Recipes</h1>
+        <h4>No fluff, just recipes</h4>
+      </div>
+    </div>
+  </header>
 </template>
+<style scoped lang="scss">
+.hero {
+  height: 40vh;
+  background: url('@/assets/main.jpeg') center/cover no-repeat;
+  margin-bottom: 2rem;
+  border-radius: var(--borderRadius);
+  position: relative;
 
-<script>
-export default {
-  name: "Hero"
+  .hero-container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.4);
+    border-radius: var(--borderRadius);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .hero-text {
+      text-align: center;
+      color: var(--white);
+    }
+
+    @media screen and (min-width: 768px) {
+      .hero-text h1 {
+        font-size: 4rem;
+      }
+    }
+  }
 }
-</script>
-
-<style scoped>
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
+  <Hero/>
 </template>
 
 <style lang="scss">
 
 </style>
+<script setup>
+import Hero from "../components/Home/Hero.vue";
+</script>
