ff777ffdf5b14f020d3f96e4e69c2cb260a70df9
Anuj Shrestha <anuj@studiomatrix.com.np>
2023-03-03 11:30:08 +0545

    feat(404): add a 404 page designs

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,4 +1,20 @@
 <template>
-  <h1>NOT FOUND</h1>
+  <div class="error-page">
+    <section>
+      <h1>404</h1>
+      <h3>Page Not found</h3>
+    </section>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.error-page {
+  text-align: center;
+  padding-top: 5rem;
+
+  h1 {
+    font-size: 9rem;
+  }
+}
+</style>
 
